b14e59f41ec2a0e4e4dea4e5f29816833f8b3a16
Ricardo <altamiranoricardo545@gmail.com>
2021-12-26 20:49:58 -0600

    Añadimos section de boletos y corregimos la seccion de galeria y añadimos un footer error en el grid de boletos corregir

--- src/js/app.js
+++ src/js/app.js
@@ -9,39 +9,62 @@ CreandoGaleria();
 
 const CreandoGaleria=()=>{
 const galeria=document.querySelector('.galeria-imagenes');
-for(let i=1; i<=12 ;i++){
+for(let i=1; i<=12; i++){
 //mostramos las imagenes debemos meter el createElemet adentro para hacer una de cada imagen y luego guardarlass
-const imagenes=document.createElement('picture');//creamos un elemento para meter las imangenes cone l appendchield
-imagenes.innerHTML=`
+const imagen=document.createElement('picture');//creamos un elemento para meter las imangenes cone l appendchield
+imagen.innerHTML=`
    <source srcset="build/imagenes/thumb/${i}.avif" type="image/avif">
                 <source srcset="build/imagenes/thumb/${i}.webp" type="image/webp">
               
                 <img loading="lazy" witdh="200" heigth="300" src="buil/imagenes/thumb/${i}.jpg" />
 
-`
-
+`;
+galeria.appendChild(imagen);//añadimos las imagenes con el appendchield
 //para crear una imagen grande cuando de click 
 //creamos un callback para poder enviar el parametro
-imagenes.onclick=function (){
+imagen.addEventListener('click',()=>{mostrandoImagen(i)});
 
-mostrandoImagen(i)
-}
-galeria.appendChild(imagenes);//añadimos las imagenes con el appendchield
 }
 }
 //Creamos la funcion para mostrar las imagenes en grande
 function mostrandoImagen(indice){
 
-const imagenes=document.createElement('picture');//creamos un elemento para meter las imangenes cone l appendchield
-imagenes.innerHTML=`
-   <source srcset="build/imagenes/grande/${indice}.avif" type="image/avif">
-                <source srcset="build/imagenes/grande/${indice}.webp" type="image/webp">
-              
-                <img loading="lazy" witdh="200" heigth="300" src="buil/imagenes/grande/${indice}.jpg" />
+  const imagen = document.createElement('picture');
+    imagen.innerHTML = `
+        <source srcset="build/imagenes/grande/${indice}.avif" type="image/avif">
+        <source srcset="build/imagenes/grande/${indice}.webp" type="image/webp">
+        <img loading="lazy" width="200" height="300" src="build/imagenes/grande/${indice}.jpg" alt="imagen galeria">
+    `;
+ 
+  //para mostrar la imagen creamo un overlay 
+   const overlay=document.createElement('div');
+   overlay.classList.add('overlay');//añadimos una clase a nuestro elemento
+    //para quitar el scroll y dando click se quite la imagen
+   overlay.onclick=()=>{
+      const body=document.querySelector('body');
+    body.classList.remove('fijar-scroll');
+     //cerramos laimagen 
+     overlay.remove(); 
+    }
+   overlay.appendChild(imagen);
+   //Creamos el boton de cerrar
+    const cerrarModal=document.createElement('p');
+    cerrarModal.textContent="X";
+    cerrarModal.classList.add('btn-cerrar');
+    cerrarModal.onclick=()=>{
+    //para desactivar el scrool y que siga funcionando
+    const body=document.querySelector('body');
+    body.classList.remove('fijar-scroll');
+     //cerramos laimagen 
+     overlay.remove();
+    }
+    overlay.appendChild(cerrarModal);
 
-`
-
-console.log(imagenes);
+  //para mostrar la imagen enla pantalla 
+  const body=document.querySelector('body');
+   body.appendChild(overlay);
+  //agregamos una clase para fijar el scroll
+   body.classList.add("fijar-scroll");
 
 }
 
